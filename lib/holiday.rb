539fa83fc72d34314d5702b31e7343186adaf6d1
idith almog <almog.idith@gmail.com>
2020-05-03 21:05:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,13 +75,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  BBQ=[]
+  bbq=[]
 holiday_hash.each do |season,season_hash|
   season_hash.each do |holiday,supplies|
     if supplies.any? == "BBQ"
-      BBQ << holiday.to_s
+      bbq << holiday.to_s
     end
   end
 end
-BBQ
+bbq
 end
